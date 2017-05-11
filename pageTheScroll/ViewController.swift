b36f7ba0217a87ea556b14df11045684d7e92549
neilmallory <neilmallory@neils-macbook-pro.local>
2017-05-11 17:37:44 +0100

    Lecture completed

--- pageTheScroll/ViewController.swift
+++ pageTheScroll/ViewController.swift
@@ -18,22 +18,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    
-        for x in 0...2 {
-            let image = UIImage(named: "icon\(x).png")
-            let imageView = UIImageView(image: image)
-            images.append(imageView)
-            
-            var newX: CGFloat = 0.0
-            
-            newX = view.frame.midX + (view.frame.size.width * CGFloat(x))
-            
-            scrollView.addSubview(imageView)
-            
-            imageView.frame = CGRect(x: newX - 75, y: (view.frame.size.height / 2) - 75, width: 150, height: 150)
-        }
-        
-        print("Count: \(images.count)")
         
     }
 
@@ -42,6 +26,39 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
 
+        var contentWidth = CGFloat(0.0)
+        
+        print("Scrollview width: \(scrollView.frame.size.width)")
+        
+        let scrollWidth = scrollView.frame.size.width
+        
+        for x in 0...2 {
+            let image = UIImage(named: "icon\(x).png")
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            
+            var newX: CGFloat = 0.0
+            
+            newX = scrollWidth / 2 + (scrollWidth * CGFloat(x))
+            
+            contentWidth += newX
+            
+            scrollView.addSubview(imageView)
+            
+            imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height / 2) - 75, width: 150, height: 150)
+        }
+        
+        print("Count: \(images.count)")
+        
+        //scrollView.backgroundColor = UIColor.purple
+        scrollView.clipsToBounds = false
+        
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
+        
+    }
+    
 }
 
